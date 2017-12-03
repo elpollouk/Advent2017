5e4cd851761d92b8fc5079f596b93824bf9eed80
Adrian O'Grady <elpollouk@gmail.com>
2017-12-03 00:11:01 -0800

    Dat 3 Problem 2
    This one sucked

--- Adevent2017/Problem0301.cs
+++ Adevent2017/Problem0301.cs
@@ -62,5 +62,90 @@ namespace Adevent2017
 
         [Fact]
         public void Solution01() => Solve01(277678).Should().Be(475);
+
+        int SumNeighbours(int[,] grid, int x, int y)
+        {
+            var sum = 0;
+            sum += grid[x - 1, y - 1];
+            sum += grid[x    , y - 1];
+            sum += grid[x + 1, y - 1];
+            sum += grid[x + 1, y    ];
+            sum += grid[x + 1, y + 1];
+            sum += grid[x    , y + 1];
+            sum += grid[x - 1, y + 1];
+            sum += grid[x - 1, y    ];
+            return sum;
+        }
+
+        [Fact]
+        public void Solution02()
+        {
+            int target = 277678;
+
+            // Just brute force it so I can go back to drinking
+            int width = 25;
+            // Zero the grid
+            int[,] grid = new int[width, width];
+            for (var i = 0; i < width; i++)
+                for (var j = 0; j < width; j++)
+                    grid[i, j] = 0;
+
+            // Starting pos
+            int x = width / 2;
+            int y = width / 2;
+
+            // Explicitly set the initial position to seed everything
+            grid[x, y] = 1;
+
+            var quadrantLength = 0;
+
+            for (var ring = 1; ring < width / 2; ring++)
+            {
+                var dX = 0;
+                var dY = -1;
+                // This is a fudge to move the current write position to a place that makes the first increment work
+                x++;
+                y++;
+                quadrantLength += 2;
+
+                for (var quadrant = 0; quadrant < 4; quadrant++)
+                {
+                    for (var i = 0; i < quadrantLength; i++)
+                    {
+                        x += dX;
+                        y += dY;
+
+                        grid[x, y] = SumNeighbours(grid, x, y);
+                        if (grid[x, y] > target)
+                        {
+                            // And the problem was looking for the value not the cell number being written to (faceplam)
+                            grid[x, y].Should().Be(279138);
+                            return;
+                        }
+                    }
+
+                    // Update the deltas for the next quadrant
+                    if (dY == -1)
+                    {
+                        dX = -1;
+                        dY = 0;
+                    }
+                    else if (dX == -1)
+                    {
+                        dX = 0;
+                        dY = 1;
+                    }
+                    else if (dY == 1)
+                    {
+                        dX = 1;
+                        dY = 0;
+                    }
+                    
+                    // No need to do anything after the last quadrant as we explicitly set dX/dY at the start of the next ring
+                }
+            }
+
+            throw new Exception();
+        }
     }
 }
