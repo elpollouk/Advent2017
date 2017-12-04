3371c8696a26f46d31023d1ff46815bac3fd342c
Adrian O'Grady <elpollouk@users.noreply.github.com>
2017-12-03 21:52:08 -0800

    Day 4 Problem 2

--- Adevent2017/Problem0401.cs
+++ Adevent2017/Problem0401.cs
@@ -21,6 +21,49 @@ namespace Adevent2017
             return true;
         }
 
+        int[] CreateSignature(string word)
+        {
+            word = word.ToLowerInvariant();
+            var signature = new int[26];
+            for (var i = 0; i < word.Length; i++)
+                signature[word[i] - 'a']++;
+
+            return signature;
+        }
+
+        bool HasSignature(List<int[]> signatures, int[] signature)
+        {
+            foreach (var s in signatures)
+            {
+                for (var i = 0; i < 26; i++)
+                {
+                    if (s[i] != signature[i])
+                        goto nextsig;
+                }
+                return true;
+            nextsig:;
+            }
+
+            return false;
+        }
+
+        bool IsValid2(string phrase)
+        {
+            var signatureList = new List<int[]>();
+            var words = phrase.Split(' ');
+
+            foreach (var word in words)
+            {
+                var signature = CreateSignature(word);
+                if (HasSignature(signatureList, signature))
+                    return false;
+
+                signatureList.Add(signature);
+            }
+
+            return true;
+        }
+
         [Theory]
         [InlineData("aa bb cc dd ee", true)]
         [InlineData("aa bb cc dd aa", false)]
@@ -38,6 +81,28 @@ namespace Adevent2017
                     count++;
 
             count.Should().Be(466);
+        }
+
+        [Theory]
+        [InlineData("abcde fghij", true)]
+        [InlineData("abcde xyz ecdab", false)]
+        [InlineData("a ab abc abd abf abj", true)]
+        [InlineData("iii oiii ooii oooi oooo", true)]
+        [InlineData("oiii ioii iioi iiio", false)]
+        public void Example02(string input, bool answer) => IsValid2(input).Should().Be(answer);
+
+        [Fact]
+        public void Solution02()
+        {
+            var reader = new StreamReader("data/0401.txt");
+            string phrase;
+            var count = 0;
+            while ((phrase = reader.ReadLine()) != null)
+                if (IsValid2(phrase))
+                    count++;
+
+            count.Should().Be(251);
+
         }
     }
 }
