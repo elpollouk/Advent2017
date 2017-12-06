6c8154c7dd9e801819592f31e705bdaf39a78ae1
Adrian O'Grady <elpollouk@gmail.com>
2017-12-06 00:24:44 -0800

    Code elegance

--- Adevent2017/Problem0601.cs
+++ Adevent2017/Problem0601.cs
@@ -9,6 +9,9 @@ namespace Adevent2017
 {
     public class Problem0601
     {
+        static int Part1(int count, int historic) => count;
+        static int Part2(int count, int historic) => count - historic;
+
         int MaxIndex(int[] input)
         {
             var max = input[0];
@@ -47,13 +50,7 @@ namespace Adevent2017
                 input[i++ % len] += v;
             }
 
-            var builder = new StringBuilder();
-            foreach (var v in input)
-            {
-                builder.Append(v);
-                builder.Append(',');
-            }
-            return builder.ToString();
+            return input.Join();
         }
 
         public int Solve(Func<int, int, int> resultor, params int[] input)
@@ -71,23 +68,23 @@ namespace Adevent2017
         }
 
         [Fact]
-        public void Example1() =>Solve((c, h) => c, 0, 2, 7, 0).Should().Be(5);
+        public void Example1() =>Solve(Part1, 0, 2, 7, 0).Should().Be(5);
 
         [Fact]
         public void Solution1()
         {
             var data = FileIterator.LoadLines<int>("Data/0601.txt");
-            Solve((c, h) => c, data).Should().Be(14029);
+            Solve(Part1, data).Should().Be(14029);
         }
 
         [Fact]
-        public void Example2() => Solve((c, h) => c - h, 0, 2, 7, 0).Should().Be(4);
+        public void Example2() => Solve(Part2, 0, 2, 7, 0).Should().Be(4);
 
         [Fact]
         public void Solution2()
         {
             var data = FileIterator.LoadLines<int>("Data/0601.txt");
-            Solve((c, h) => c - h, data).Should().Be(2765);
+            Solve(Part2, data).Should().Be(2765);
         }
     }
 }
---
+++ Adevent2017/Utils/ArrayExtensions.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Text;
+
+namespace Adevent2017.Utils
+{
+    static class ArrayExtensions
+    {
+        public static string Join<T>(this T[] a, string joiner = ",")
+        {
+            if (a.Length == 0) return string.Empty;
+
+            var builder = new StringBuilder();
+            builder.Append(a[0]);
+            for (var i = 1; i < a.Length; i++)
+            {
+                builder.Append(joiner);
+                builder.Append(a[i]);
+            }
+            return builder.ToString();
+        }
+    }
+}
