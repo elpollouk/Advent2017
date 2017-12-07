438d6995051438c2fafe76b57fdec4603537a5d6
Adrian O'Grady <elpollouk@users.noreply.github.com>
2017-12-06 22:49:17 -0800

    Day 7 Problem 2

--- Adevent2017/Problem0701.cs
+++ Adevent2017/Problem0701.cs
@@ -8,30 +8,111 @@ namespace Adevent2017
 {
     public class Problem0701
     {
-        class Node
+        public class Node
         {
             public string Name;
             public string Parent;
-            public string[] Children;
+            public string[] ChildNames;
+            public List<Node> ChildNodes = new List<Node>();
+            public int Weight;
+            public int SummedWeight;
+        }
+
+        // I can't think of a better name at this time of night
+        class SumCache
+        {
+            public int Count;
+            public Node Node;
         }
 
         Node ParseLine(string line)
         {
+            // Bollocks to regex
             var backFront = line.Split('>');
             var name = backFront[0].Split(' ')[0];
-            // Does it have childrean
+            var weight = backFront[0].Split('(')[1].Split(')')[0];
+        
+            // Does it have children?
             string[] children = new string[0];
             if (backFront.Length == 2)
-                children = backFront[1].Split(new char[] { ',', ' '}, StringSplitOptions.RemoveEmptyEntries);
+                children = backFront[1].Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
 
             return new Node()
             {
                 Name = name,
-                Children = children
+                ChildNames = children,
+                Weight = int.Parse(weight),
+                SummedWeight = 0
             };
         }
 
-        public string Solve(string[] input)
+        Node FindRoot(Dictionary<string, Node> tree)
+        {
+            foreach (var node in tree.Values)
+                if (node.Parent == null)
+                    return node;
+
+            throw new Exception("It's fucked");
+        }
+
+        int SumChildren(Node node)
+        {
+            if (node.SummedWeight != 0) return node.SummedWeight;
+
+            var sum = node.Weight;
+            foreach (var child in node.ChildNodes)
+            {
+                sum += SumChildren(child);
+            }
+
+            node.SummedWeight = sum;
+
+            return sum;
+        }
+
+        int FindWeightAdjustment(Node node)
+        {
+            // We're looking for the most leafy answer it turns out
+            foreach (var child in node.ChildNodes)
+            {
+                var adjustment = FindWeightAdjustment(child);
+                if (adjustment != 0) return adjustment;
+            }
+
+            var sums = new Dictionary<int, SumCache>();
+
+            foreach (var child in node.ChildNodes)
+            {
+                var sum = SumChildren(child);
+                if (!sums.ContainsKey(sum)) sums[sum] = new SumCache(){ Count = 0, Node = child };
+                sums[sum].Count++;
+            }
+
+            var oddWeight = -1;
+            var expectedWeight = -1;
+            foreach (var sum in sums.Keys)
+            {
+                if (sums[sum].Count == 1)
+                    oddWeight = sum;
+                else
+                    expectedWeight = sum;
+            }
+
+            if (oddWeight != -1)
+            {
+                var adjustment = (expectedWeight - oddWeight);
+                return sums[oddWeight].Node.Weight += adjustment;
+            }
+
+            return 0;
+        }
+
+        int FindFixedWeight(Node tree)
+        {
+            return FindWeightAdjustment(tree);
+        }
+
+        public Node BuildTree(string[] input)
         {
             var tree = new Dictionary<string, Node>();
             foreach (var line in input)
@@ -43,30 +124,35 @@ namespace Adevent2017
             // Resolve
             foreach (var node in tree.Values)
             {
-                foreach (var child in node.Children)
-                    tree[child].Parent = node.Name;
+                foreach (var child in node.ChildNames)
+                {
+                    var childNode = tree[child];
+                    childNode.Parent = node.Name;
+                    node.ChildNodes.Add(childNode);
+                }
             }
 
-            // Search
-            foreach (var node in tree.Values)
-                if (node.Parent == null)
-                    return node.Name;
-
-            return "";
+            return FindRoot(tree);
         }
 
-        [Fact]
-        public void Example1()
+        [Theory]
+        [InlineData("Data/0701-example.txt", "tknk")]
+        [InlineData("Data/0701.txt", "azqje")]
+        public void Part1(string input, string answer)
         {
-            var lines = FileIterator.LoadLines<string>("Data/0701-example.txt");
-            Solve(lines).Should().Be("tknk");
+            var lines = FileIterator.LoadLines<string>(input);
+            var tree = BuildTree(lines);
+            tree.Name.Should().Be(answer);
         }
 
-        [Fact]
-        public void Solution1()
+        [Theory]
+        [InlineData("Data/0701-example.txt", 60)]
+        [InlineData("Data/0701.txt", 646)]
+        public void Part2(string input, int answer)
         {
-            var lines = FileIterator.LoadLines<string>("Data/0701.txt");
-            Solve(lines).Should().Be("azqje");
+            var lines = FileIterator.LoadLines<string>(input);
+            var tree = BuildTree(lines);
+            FindFixedWeight(tree).Should().Be(answer);
         }
     }
 }
