d141e0282a2d0854c7a59f5c2dc553fa43a41dd1
Adrian O'Grady <elpollouk@gmail.com>
2017-12-07 19:31:11 -0800

    Day 8 prep

---
+++ Adevent2017/Problem0801.cs
@@ -0,0 +1,29 @@
+﻿using Adevent2017.Utils;
+using FluentAssertions;
+using Xunit;
+
+namespace Adevent2017
+{
+    public class Problem0801
+    {
+        int Solve(string input)
+        {
+            return -1;
+        }
+
+        [Theory]
+        [InlineData("Data/0701-example.txt", 0)]
+        [InlineData("Data/0701.txt", 0)]
+        public void Part1(string input, int answer)
+        {
+            Solve(input).Should().Be(answer);
+        }
+
+        [Theory]
+        [InlineData("Data/0701-example.txt", 60)]
+        [InlineData("Data/0701.txt", 646)]
+        public void Part2(string input, int answer)
+        {
+        }
+    }
+}
