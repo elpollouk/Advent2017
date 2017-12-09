d03a42f957486ea3286b11d588cc38244d5ec0e2
Adrian O'Grady <elpollouk@users.noreply.github.com>
2017-12-09 12:46:05 -0800

    Improving my French

--- Adevent2017/Utils/Oh.cs
+++ Adevent2017/Utils/Oh.cs
@@ -4,9 +4,12 @@ namespace Adevent2017.Utils
 {
     static class Oh
     {
-        public static Exception Bollocks()
-        {
-            throw new Exception("Bollocks");
-        }
+        private static void Swear(string profanity) { throw new Exception($"Oh {profanity}"); }
+
+        public static void Bollocks() => Swear("bollock");
+        public static void ForFucksSake() => Swear("for fucks sake!");
+        public static void WhatTheFuck() => Swear("what the fuck?");
+        public static void PissingNora() => Swear("pissing Nora");
+        public static void ShttingHell() => Swear("shitting hell!");
     }
 }
