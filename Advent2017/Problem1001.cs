d2ef9c23bb3c69e64014b0dbc64dc873c1245a7e
Adrian O'Grady <elpollouk@gmail.com>
2017-12-09 19:28:52 -0800

    Day 10 prep

---
+++ Advent2017/Problem1001.cs
@@ -0,0 +1,22 @@
+﻿using Adevent2017.Utils;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Adevent2017
+{
+    public class Problem1001
+    {
+
+        public void Part1(string input, int answer)
+        {
+
+        }
+
+        public void Part2(string input, int answer)
+        {
+
+        }
+    }
+}
