c6525a417c9c4de8825b7ccc9ab2da3c9fdf3f04
Adrian O'Grady <elpollouk@users.noreply.github.com>
2017-12-13 19:38:35 -0800

    Day 14 prep

---
+++ Advent2017/Problem1401.cs
@@ -0,0 +1,30 @@
+﻿using Adevent2017.Utils;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Adevent2017
+{
+    public class Problem1401
+    {
+        [Theory]
+        [InlineData("Data/1401-example.txt", 24)]
+        [InlineData("Data/1401.txt", 2160)]
+        public void Part1(string datafile, int answer)
+        {
+            var lines = FileIterator.LoadLines<string>(datafile);
+            lines.Length.Should().Be(answer);
+        }
+
+        [Theory]
+        [InlineData("Data/1401-example.txt", 10)]
+        [InlineData("Data/1401.txt", 3907470)]
+        public void Part2(string datafile, int answer)
+        {
+            var lines = FileIterator.LoadLines<string>(datafile);
+            lines.Length.Should().Be(answer);
+        }
+    }
+}
