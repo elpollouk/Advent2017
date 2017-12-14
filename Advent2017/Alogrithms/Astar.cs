ecee38400c5ea73d8bccba4c2274e65d93107a33
Adrian O'Grady <elpollouk@users.noreply.github.com>
2017-12-13 19:36:40 -0800

    Working on A*

---
+++ Advent2017/Alogrithms/Astar.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adevent2017.Alogrithms
+{
+    static class Astar<Node, Graph>
+    {
+        public interface IGraph
+        {
+            IList<Node> GetChildrean(Node node);
+            int GetMoveCost(Node node1, Node node2);
+            int GetScore(Node node);
+        }
+
+        public static IList<Node> FindPath(Graph graph, Node start, Node goal, Func<Graph, Node, IList<Node>> getChildren, )
+        {
+            var searchSpace = new PriorityQueue<Node>();
+            var pathMap = new Dictionary<Node, Node>();
+            var costSoFat = new Dictionary<Node, int>();
+
+            return ResolvePath(pathMap, start, goal);
+        }
+
+        private static IList<Node> ResolvePath(Dictionary<Node, Node> pathMap, Node start, Node goal)
+        {
+            var path = new LinkedList<Node>();
+            if (!pathMap.ContainsKey(goal)) return null;
+
+            var node = goal;
+            do
+            {
+                node = pathMap[node];
+            }
+            while (node != start);
+        }
+    }
+}
---
+++ Advent2017/Alogrithms/PriorityQueue.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adevent2017.Alogrithms
+{
+    class PriorityQueue<T, PriorityType> where PriorityType : IComparable<PriorityType>
+    {
+        private struct QueueElement
+        {
+            public readonly PriorityType priority;
+            public readonly T item;
+
+            public QueueElement(PriorityType priority, T item)
+            {
+                this.priority = priority;
+                this.item = item;
+            }
+        }
+
+        private List<QueueElement> _queue = new List<QueueElement>();
+
+        public int Count => _queue.Count;
+
+        void Enqueue(T item, PriorityType priority)
+        {
+            for (var i = 0; i < _queue.Count; i++)
+            {
+                if (_queue[i].priority.CompareTo(priority) >= 0)
+                {
+                    _queue.Insert(i, new QueueElement(priority, item));
+                    return;
+                }
+            }
+
+            _queue.Add(new QueueElement(priority, item));
+        }
+
+        T Dequeue()
+        {
+            var el = _queue.First();
+            _queue.RemoveAt(0);
+            return el.item;
+        }
+    }
+
+    class PriorityQueue<T> : PriorityQueue<T, int> { }
+}
