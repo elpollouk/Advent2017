191926bccc6ee126c554ec24517790e162d6d3f4
Adrian O'Grady <elpollouk@users.noreply.github.com>
2017-12-16 21:36:43 -0800

    Day 17

---
+++ Advent2017/Problem1701.cs
@@ -0,0 +1,73 @@
+﻿using FluentAssertions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Adevent2017
+{
+    public class Problem1701
+    {
+        List<int> Solve1(int stepSize, int numCycles)
+        {
+            var buffer = new List<int>();
+            buffer.Add(0);
+
+            int currentPosition = 0;
+            for (var i = 0; i < numCycles; i++)
+            {
+                currentPosition += stepSize;
+                currentPosition = (currentPosition % buffer.Count) + 1;
+                buffer.Insert(currentPosition, i+1);
+            }
+
+            return buffer;
+        }
+
+        int Solve2(int stepSize, int numCycles)
+        {
+            var valueAt1 = -1;
+            int bufferSize = 1;
+            int currentPosition = 0;
+            for (var i = 0; i < numCycles; i++)
+            {
+                currentPosition += stepSize;
+                currentPosition = (currentPosition % bufferSize) + 1;
+                if (currentPosition == 1)
+                    valueAt1 = i + 1;
+                bufferSize++;
+            }
+
+            return valueAt1;
+        }
+
+        [Fact]
+        public void Part1Example()
+        {
+            var buffer = Solve1(3, 9);
+            buffer.Count.Should().Be(10);
+            buffer[0].Should().Be(0);
+            buffer[1].Should().Be(9);
+            buffer[2].Should().Be(5);
+            buffer[3].Should().Be(7);
+            buffer[4].Should().Be(2);
+            buffer[5].Should().Be(4);
+            buffer[6].Should().Be(3);
+            buffer[7].Should().Be(8);
+            buffer[8].Should().Be(6);
+            buffer[9].Should().Be(1);
+        }
+
+        [Fact]
+        public void Part1Solution()
+        {
+            var buffer = Solve1(380, 2017);
+            buffer.Count.Should().Be(2018);
+            var i = buffer.IndexOf(2017);
+            buffer[i + 1].Should().Be(204);
+        }
+
+        [Theory]
+        [InlineData(3, 9, 9)]
+        [InlineData(380, 50000000, 28954211)]
+        public void Part2Solution(int stepSize, int numCycles, int answer) => Solve2(stepSize, numCycles).Should().Be(answer);
+    }
+}
