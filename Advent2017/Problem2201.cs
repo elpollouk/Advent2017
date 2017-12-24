ce71454767804ffec7c367b457651a7130a925a1
Adrian O'Grady <elpollouk@users.noreply.github.com>
2017-12-23 22:01:01 -0800

    Day 22 Part 2

--- Advent2017/Problem2201.cs
+++ Advent2017/Problem2201.cs
@@ -1,6 +1,5 @@
 ﻿using Adevent2017.Utils;
 using FluentAssertions;
-using System;
 using System.Collections.Generic;
 using Xunit;
 
@@ -23,14 +22,11 @@ namespace Adevent2017
             return (ux << 32) | (uy & 0xFFFFFFFF);
         }
 
-        int Solve1(string datafile, int cycles)
+        Dictionary<ulong, NodeState> LoadGrid(string datafile, out int startX, out int startY)
         {
             var grid = new Dictionary<ulong, NodeState>();
-            var dy = -1;
-            var dx = 0;
             var x = 0;
             var y = 0;
-            var infections = 0;
 
             FileIterator.ForEachLine<string>(datafile, line =>
             {
@@ -46,8 +42,40 @@ namespace Adevent2017
                 y++;
             });
 
-            x /= 2;
-            y /= 2;
+            startX = x / 2;
+            startY = y / 2;
+
+            return grid;
+        }
+
+        void TurnLeft(ref int dx, ref int dy)
+        {
+            var t = dx;
+            dx = dy;
+            dy = -t;
+        }
+
+        void TurnRight(ref int dx, ref int dy)
+        {
+            var t = dx;
+            dx = -dy;
+            dy = t;
+        }
+
+        void Reverse(ref int dx, ref int dy)
+        {
+            dx = -dx;
+            dy = -dy;
+        }
+
+        int Solve1(string datafile, int cycles)
+        {
+            int x;
+            int y;
+            var dy = -1;
+            var dx = 0;
+            var infections = 0;
+            var grid = LoadGrid(datafile, out x, out y);
 
             for (var i = 0; i < cycles; i++)
             {
@@ -55,22 +83,59 @@ namespace Adevent2017
                 var state = grid.GetOrDefault(ec, NodeState.Clean);
                 if (state == NodeState.Infected)
                 {
-                    var t = dx;
-                    dx = -dy;
-                    dy = t;
+                    TurnRight(ref dx, ref dy);
                     grid.Remove(ec);
                 }
                 else
                 {
-                    var t = dx;
-                    dx = dy;
-                    dy = -t;
+                    TurnLeft(ref dx, ref dy);
                     grid[ec] = NodeState.Infected;
                     infections++;
                 }
                 x += dx;
                 y += dy;
+            }
 
+            return infections;
+        }
+
+        int Solve2(string datafile, int cycles)
+        {
+            int x;
+            int y;
+            var dy = -1;
+            var dx = 0;
+            var infections = 0;
+            var grid = LoadGrid(datafile, out x, out y);
+
+            for (var i = 0; i < cycles; i++)
+            {
+                var ec = EncodeCoords(x, y);
+                var state = grid.GetOrDefault(ec, NodeState.Clean);
+                switch (state)
+                {
+                    case NodeState.Clean:
+                        TurnLeft(ref dx, ref dy);
+                        grid[ec] = NodeState.Weakened;
+                        break;
+
+                    case NodeState.Weakened:
+                        grid[ec] = NodeState.Infected;
+                        infections++;
+                        break;
+
+                    case NodeState.Infected:
+                        TurnRight(ref dx, ref dy);
+                        grid[ec] = NodeState.Flagged;
+                        break;
+
+                    case NodeState.Flagged:
+                        Reverse(ref dx, ref dy);
+                        grid.Remove(ec);
+                        break;
+                }
+                x += dx;
+                y += dy;
             }
 
             return infections;
@@ -82,5 +147,11 @@ namespace Adevent2017
         [InlineData("Data/2201-example.txt", 10000, 5587)]
         [InlineData("Data/2201.txt", 10000, 5182)]
         void Part1(string datafile, int cycles, int answer) => Solve1(datafile, cycles).Should().Be(answer);
+
+        [Theory]
+        [InlineData("Data/2201-example.txt", 100, 26)]
+        [InlineData("Data/2201-example.txt", 10000000, 2511944)]
+        [InlineData("Data/2201.txt", 10000000, 2512008)]
+        void Part2(string datafile, int cycles, int answer) => Solve2(datafile, cycles).Should().Be(answer);
     }
 }
