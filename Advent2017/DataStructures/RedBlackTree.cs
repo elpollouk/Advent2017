5b98cb42b66bbee8d83dfa0e7b9c8b2a8835fb56
Adrian O'Grady <adriano@microsoft.com>
2018-02-22 16:33:26 -0800

    Added a red black tree implementation

---
+++ Advent2017/DataStructures/RedBlackTree.cs
@@ -0,0 +1,212 @@
+﻿using System;
+
+namespace Adevent2017.DataStructures
+{
+    enum Colour
+    {
+        Red,
+        Black,
+    }
+
+    class RedBlackTree<Key, Value> where Key : IComparable
+    { 
+        public class Node
+        {
+            public Node(Key key, Value value)
+            {
+                this.Key = key;
+                this.Value = value;
+            }
+
+            public readonly Key Key;
+            public readonly Value Value;
+            public Colour Colour = Colour.Red;
+             
+            public Node Parent = null;
+            public Node Left = null;
+            public Node Right = null;
+
+
+            public Node Sibling
+            {
+                get
+                {
+                    if (Parent == null)
+                        return null;
+                    if (this == Parent.Left)
+                        return Parent.Right;
+                    return Parent.Left;
+                }
+            }
+
+            public Node Uncle
+            {
+                get
+                {
+                    if (Parent == null)
+                        return null;
+                    return Parent.Sibling;
+                }
+            }
+
+            public Node GrandParent
+            {
+                get
+                {
+                    if (Parent == null)
+                        return null;
+                    return Parent.Parent;
+                }
+            }
+
+            public void RotateLeft()
+            {
+                var t = Right;
+                Right = t.Left;
+                t.Left = this;
+                t.Parent = Parent;
+                Parent = t;
+
+                if (t.Parent != null)
+                {
+                    if (t.Parent.Left == this)
+                        t.Parent.Left = t;
+                    else
+                        t.Parent.Right = t;
+                }
+            }
+
+            public void RotateRight()
+            {
+                var t = Left;
+                Left = t.Right;
+                t.Right = this;
+                t.Parent = Parent;
+                Parent = t;
+
+                if (t.Parent != null)
+                {
+                    if (t.Parent.Left == this)
+                        t.Parent.Left = t;
+                    else
+                        t.Parent.Right = t;
+                }
+            }
+
+            public override string ToString()
+            {
+                return $"Key: {Key}, Value: {Value}";
+            }
+        }
+
+        public Node _root = null;
+
+        public void Insert(Key key, Value value)
+        {
+            var node = new Node(key, value);
+            InsertInternal(_root, node);
+            Repair(node);
+
+            _root = node;
+            while (_root.Parent != null)
+                _root = _root.Parent;
+        }
+
+        public Value Search(Key key)
+        {
+            return SearchInternal(_root, key);
+        }
+
+        public Value SearchInternal(Node node, Key key)
+        {
+            if (node == null)
+                return default(Value);
+
+            var compareResult = key.CompareTo(node.Key);
+            if (compareResult == 0)
+                return node.Value;
+
+            if (compareResult < 0)
+                return SearchInternal(node.Left, key);
+
+            return SearchInternal(node.Right, key);
+        }
+
+        private void InsertInternal(Node parent, Node node)
+        {
+            if (parent == null) return; // This is the new root
+
+            var compareResult = node.Key.CompareTo(parent.Key);
+            if (compareResult < 0)
+            {
+                if (parent.Left != null)
+                {
+                    InsertInternal(parent.Left, node);
+                }
+                else
+                {
+                    parent.Left = node;
+                    node.Parent = parent;
+                }
+            }
+            else
+            {
+                if (parent.Right != null)
+                {
+                    InsertInternal(parent.Right, node);
+                }
+                else
+                {
+                    parent.Right = node;
+                    node.Parent = parent;
+                }
+            }
+        }
+
+        private void Repair(Node node)
+        {
+            if (node.Parent == null)
+            {
+                // Root node should always be black
+                node.Colour = Colour.Black;
+            }
+            else if (node.Parent.Colour == Colour.Black)
+            {
+                // Do nothing
+            }
+            else if (node.Uncle != null && node.Uncle.Colour == Colour.Red)
+            {
+                // Parent and uncle are both red, make the colours align with the current node
+                node.Parent.Colour = Colour.Black;
+                node.Uncle.Colour = Colour.Black;
+                node.GrandParent.Colour = Colour.Red;
+                Repair(node.GrandParent);
+            }
+            else
+            {
+                // ????
+                if (node.GrandParent.Left != null && node == node.GrandParent.Left.Right)
+                {
+                    node.Parent.RotateLeft();
+                    node = node.Left;
+                }
+                else if (node.GrandParent.Right != null && node == node.GrandParent.Right.Left)
+                {
+                    node.Parent.RotateRight();
+                    node = node.Right;
+                }
+
+                var p = node.Parent;
+                var g = node.GrandParent;
+
+                if (node == p.Left)
+                    g.RotateRight();
+                else
+                    g.RotateLeft();
+
+                p.Colour = Colour.Black;
+                g.Colour = Colour.Red;
+            }
+        }
+    }
+}
---
+++ Advent2017/DataStructures/RedBlackTreeTests.cs
@@ -0,0 +1,100 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Adevent2017.DataStructures
+{
+    public class RedBlackTreeTests
+    {
+        static readonly RedBlackTree<int, string>.Node Leaf = null;
+
+        void VerifyNode(RedBlackTree<int, string>.Node node, string value, string parentValue, string leftValue, string rightValue)
+        {
+            node.Value.Should().Be(value);
+
+            if (parentValue != null)
+                node.Parent.Value.Should().Be(parentValue);
+            else
+                node.Parent.Should().BeNull();
+
+            if (leftValue != null)
+                node.Left.Value.Should().Be(leftValue);
+            else
+                node.Left.Should().Be(Leaf);
+
+            if (rightValue != null)
+                node.Right.Value.Should().Be(rightValue);
+            else
+                node.Right.Should().Be(Leaf);
+        }
+
+        [Fact]
+        public void Insert()
+        {
+            var tree = new RedBlackTree<int, string>();
+            tree.Insert(5, "A");
+            VerifyNode(tree._root, "A", null, null, null);
+
+            tree.Insert(4, "B");
+            VerifyNode(tree._root, "A", null, "B", null);
+            VerifyNode(tree._root.Left, "B", "A", null, null);
+
+            tree.Insert(3, "C");
+            VerifyNode(tree._root, "B", null, "C", "A");
+            VerifyNode(tree._root.Left, "C", "B", null, null);
+            VerifyNode(tree._root.Right, "A", "B", null, null);
+
+            tree.Insert(2, "D");
+            VerifyNode(tree._root, "B", null, "C", "A");
+            VerifyNode(tree._root.Left, "C", "B", "D", null);
+            VerifyNode(tree._root.Right, "A", "B", null, null);
+            VerifyNode(tree._root.Left.Left, "D", "C", null, null);
+
+            tree.Insert(1, "E");
+            VerifyNode(tree._root, "B", null, "D", "A");
+            VerifyNode(tree._root.Left, "D", "B", "E", "C");
+            VerifyNode(tree._root.Right, "A", "B", null, null);
+            VerifyNode(tree._root.Left.Left, "E", "D", null, null);
+            VerifyNode(tree._root.Left.Right, "C", "D", null, null);
+
+            tree.Insert(6, "F");
+            VerifyNode(tree._root, "B", null, "D", "A");
+            VerifyNode(tree._root.Left, "D", "B", "E", "C");
+            VerifyNode(tree._root.Right, "A", "B", null, "F");
+            VerifyNode(tree._root.Left.Left, "E", "D", null, null);
+            VerifyNode(tree._root.Left.Right, "C", "D", null, null);
+            VerifyNode(tree._root.Right.Right, "F", "A", null, null);
+
+            tree.Insert(7, "G");
+            VerifyNode(tree._root, "B", null, "D", "F");
+            VerifyNode(tree._root.Left, "D", "B", "E", "C");
+            VerifyNode(tree._root.Right, "F", "B", "A", "G");
+            VerifyNode(tree._root.Left.Left, "E", "D", null, null);
+            VerifyNode(tree._root.Left.Right, "C", "D", null, null);
+            VerifyNode(tree._root.Right.Left, "A", "F", null, null);
+            VerifyNode(tree._root.Right.Right, "G", "F", null, null);
+        }
+
+
+        [Fact]
+        public void Search()
+        {
+            var tree = new RedBlackTree<int, string>();
+            tree.Insert(5, "A");
+            tree.Insert(4, "B");
+            tree.Insert(3, "C");
+            tree.Insert(2, "D");
+            tree.Insert(1, "E");
+            tree.Insert(7, "F");
+            tree.Insert(6, "G");
+
+            tree.Search(1).Should().Be("E");
+            tree.Search(2).Should().Be("D");
+            tree.Search(3).Should().Be("C");
+            tree.Search(4).Should().Be("B");
+            tree.Search(5).Should().Be("A");
+            tree.Search(6).Should().Be("G");
+            tree.Search(7).Should().Be("F");
+            tree.Search(8).Should().BeNull();
+        }
+    }
+}
