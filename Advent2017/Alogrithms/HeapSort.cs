b5f01c2c76345f5561bac7504f687070f16ddf4e
Adrian O'Grady <elpollouk@gmail.com>
2018-02-23 17:45:17 -0800

    Added heap sort implementation

---
+++ Advent2017/Alogrithms/HeapSort.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Adevent2017.Alogrithms
+{
+    static class HeapSort
+    {
+        public static void Sort<T>(IList<T> collection, Func<T, T, int> compare)
+        {
+            BuildHeap(collection, compare);
+
+            for (var i = collection.Count - 1; i > 0; i--)
+            {
+                // Move the highest index from the start of the array to where the end of the unsorted list is
+                Swap(collection, 0, i);
+
+                // Rebuild the heap
+                var index = 0;
+                while (true)
+                {
+                    var leftIndex = Left(index);
+                    var rightIndex = Right(index);
+                    if (ShouldPromote(collection, compare, i, index, leftIndex) || ShouldPromote(collection, compare, i, index, rightIndex))
+                    {
+                        if (ShouldPromote(collection, compare, i, leftIndex, rightIndex))
+                        {
+                            Swap(collection, rightIndex, index);
+                            index = rightIndex;
+                        }
+                        else
+                        {
+                            Swap(collection, leftIndex, index);
+                            index = leftIndex;
+                        }
+                    }
+                    else break;
+                }
+
+            }
+        }
+
+        private static void BuildHeap<T>(IList<T> collection, Func<T, T, int> compare)
+        {
+            for (var heapSize = 0; heapSize < collection.Count; heapSize++)
+            {
+                var index = heapSize;
+                while (index != 0)
+                {
+                    var parentIndex = Parent(index);
+                    if (!ShouldPromote(collection, compare, heapSize + 1, parentIndex, index))
+                        break;
+
+                    Swap(collection, parentIndex, index);
+                    index = parentIndex;
+                }
+            }
+        }
+
+        private static void Swap<T>(IList<T> collection, int index1, int index2)
+        {
+            var t = collection[index1];
+            collection[index1] = collection[index2];
+            collection[index2] = t;
+        }
+
+        private static int Parent(int index) => index / 2;
+        private static int Left(int index) => index * 2;
+        private static int Right(int index) => (index * 2) + 1;
+
+        private static bool ShouldPromote<T>(IList<T> collection, Func<T, T, int> compare, int limit, int higherIndex, int lowerIndex)
+        {
+            if (lowerIndex == 0) return false;
+            if (limit <= lowerIndex) return false;
+            return compare(collection[higherIndex], collection[lowerIndex]) < 0;
+        }
+    }
+}
---
+++ Advent2017/Alogrithms/HeapSortTests.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+using FluentAssertions;
+
+namespace Adevent2017.Alogrithms
+{
+    public class HeapSortTests
+    {
+        static int CompareInts_InOrder(int a, int b) => a - b;
+        static int CompareInts_ReverseOrder(int a, int b) => b - a;
+
+        [Theory]
+        [InlineData(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)]
+        [InlineData(15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)]
+        [InlineData(9, 12, 2, 13, 3, 15, 5, 1, 6, 7, 4, 11, 0, 8, 14, 10)]
+        [InlineData(15, 7, 9, 12, 13, 3, 6, 1, 2, 5, 10, 4, 11, 0, 8, 14)]
+        public void Sort_IntoOrder(params int[] input)
+        {
+            HeapSort.Sort(input, CompareInts_InOrder);
+
+            for (var i = 0; i < input.Length; i++)
+                input[i].Should().Be(i);
+        }
+
+        [Theory]
+        [InlineData(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)]
+        [InlineData(15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)]
+        [InlineData(9, 12, 2, 13, 3, 15, 5, 1, 6, 7, 4, 11, 0, 8, 14, 10)]
+        [InlineData(15, 7, 9, 12, 13, 3, 6, 1, 2, 5, 10, 4, 11, 0, 8, 14)]
+        public void Sort_IntoReverseOrder(params int[] input)
+        {
+            HeapSort.Sort(input, CompareInts_ReverseOrder);
+
+            for (var i = 0; i < input.Length; i++)
+                input[i].Should().Be(15 - i);
+        }
+    }
+}
