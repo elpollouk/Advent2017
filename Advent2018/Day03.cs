da3045b55e86c927e790d6c9a5af0a0bd54daaf3
Adrian O'Grady <adriano@microsoft.com>
2018-12-03 15:44:33 +0000

    Moved solution file so that it isn't under the same directory as the original project

---
+++ Advent2018/Day03.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace Advent2018
+{
+    public class Day03
+    {
+        [Theory]
+        [InlineData(4, "#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2")]
+        public void Problem1_Test(int expectedInches, params string[] requests)
+        {
+        }
+    }
+}
