f29cb3f0a2405a45575a95cfce4be4795dae825d
Adrian O'Grady <adriano@microsoft.com>
2018-12-03 16:44:54 +0000

    Day 3, Part 2

--- Advent2018/Day03.cs
+++ Advent2018/Day03.cs
@@ -1,5 +1,7 @@
 ﻿using FluentAssertions;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Utils;
 using Xunit;
 
@@ -9,7 +11,7 @@ namespace Advent2018
     {
         class Request
         {
-            public readonly string Id;
+            public readonly int Id;
             public readonly int[] Pos;
             public readonly int[] Size;
 
@@ -19,7 +21,7 @@ namespace Advent2018
                 if (split.Length != 5)
                     Oh.Bugger();
 
-                Id = split[0].Trim();
+                Id = int.Parse(split[0].Substring(1));
                 Pos = new int[]
                 {
                     int.Parse(split[1]),
@@ -32,38 +34,55 @@ namespace Advent2018
                 };
             }
 
-            public void Apply(int[,] cloth)
+            public void Apply(Dictionary<ValueTuple<int, int>, List<int>> cloth, HashSet<int> safeRequests)
             {
+                safeRequests.Add(Id);
+
                 for (var y = 0; y < Size[1]; y++)
                     for (var x = 0; x < Size[0]; x++)
-                        cloth[Pos[0] + x, Pos[1] + y]++;
+                    {
+                        var pos = new ValueTuple<int, int>(Pos[0] + x, Pos[1] + y);
+                        var list = cloth.GetOrCreate(pos, () => new List<int>());
+                        list.Add(Id);
+                        if (list.Count != 1)
+                        {
+                            foreach (var request in list)
+                                safeRequests.Remove(request);
+                        }
+                    }
             }
         }
 
         [Theory]
-        [InlineData(4, "#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2")]
-        public void Problem1_Test(int expectedInches, params string[] requests)
+        [InlineData(4, 3, "#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2")]
+        public void Problem_Test(int expectedInches, int expectedUniqueRequest, params string[] requests)
         {
-            var cloth = new int[1000, 1000];
+            var cloth = new Dictionary<ValueTuple<int, int>, List<int>>();
+            var safeRequests = new HashSet<int>();
+
             foreach (var request in requests)
             {
                 var r = new Request(request);
-                r.Apply(cloth);
+                r.Apply(cloth, safeRequests);
             }
 
             var overlaps = 0;
-            foreach (var value in cloth)
-                if (value > 1)
+
+            foreach (var value in cloth.Values)
+                if (value.Count > 1)
                     overlaps++;
 
             overlaps.Should().Be(expectedInches);
+
+            safeRequests.Count.Should().Be(1);
+            safeRequests.First().Should().Be(expectedUniqueRequest);
         }
 
         [Fact]
-        public void Problem1_Solution()
+        public void Problem_Solution()
         {
             var requests = FileIterator.LoadLines<string>("Day03.txt");
-            Problem1_Test(104126, requests);
+            Problem_Test(104126, 695, requests);
         }
     }
 }
