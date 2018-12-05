09a490f3e0f5cc0271dd949b7e3e2eed37a61598
Adrian O'Grady <elpollouk@users.noreply.github.com>
2018-12-05 11:24:13 +0000

    Day 5, problem 2 solution

--- Advent2018/Day05.cs
+++ Advent2018/Day05.cs
@@ -16,11 +16,13 @@ namespace Advent2018
             return -((unit - 'a') + 1);
         }
 
-        int Collapse(string input)
+        int Collapse(string input, params char[] removals)
         {
+            var set = new HashSet<char>(removals);
             var list = new List<int>();
             foreach (var unit in input)
-                list.Add(UnitToValue(unit));
+                if (!set.Contains(unit))
+                    list.Add(UnitToValue(unit));
 
             var i = 0;
             while (i < list.Count - 1)
@@ -52,6 +54,28 @@ namespace Advent2018
         {
             var input = File.ReadAllText("Data/Day05.txt");
             Collapse(input).Should().Be(9370);
+        }
+
+        [Theory]
+        [InlineData("dabAcCaCBAcCcaDA", 4)]
+        public void Problem2_Test(string input, int expectedLenth)
+        {
+            var shortest = input.Length;
+            for (var i = 0; i < 26; i++)
+            {
+                var length = Collapse(input, (char)('a' + i), (char)('A' + i));
+                if (length < shortest)
+                    shortest = length;
+            }
+
+            shortest.Should().Be(expectedLenth);
+        }
+
+        [Fact]
+        public void Problem2_Solution()
+        {
+            var input = File.ReadAllText("Data/Day05.txt");
+            Problem2_Test(input, 6390);
         }
     }
 }
