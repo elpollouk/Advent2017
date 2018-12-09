02381cd036363a451d7db7976b0dfbf7f291aacc
Adrian O'Grady <elpollouk@users.noreply.github.com>
2018-12-09 13:22:51 +0000

    Day 9, problem 1 solution

---
+++ Advent2018/Day09.cs
@@ -0,0 +1,90 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utils;
+using Xunit;
+
+namespace Advent2018
+{
+    public class Day09
+    {
+        class Marble
+        {
+            public readonly int Value;
+            public Marble Clockwise;
+            public Marble AntiClockwise;
+
+            public Marble(int value)
+            {
+                Value = value;
+                if (value == 0)
+                {
+                    Clockwise = this;
+                    AntiClockwise = this;
+                }
+            }
+
+            public void Insert(Marble marble)
+            {
+                var clock1 = Clockwise;
+                var clock2 = clock1.Clockwise;
+
+                marble.AntiClockwise = clock1;
+                marble.Clockwise = clock2;
+                clock1.Clockwise = marble;
+                clock2.AntiClockwise = marble;
+            }
+
+            public Marble RemoveAntiClockwise(uint offset)
+            {
+                var marble = this;
+                while (offset-- != 0)
+                    marble = marble.AntiClockwise;
+
+                marble.AntiClockwise.Clockwise = marble.Clockwise;
+                marble.Clockwise.AntiClockwise = marble.AntiClockwise;
+
+                return marble;
+            }
+
+            public override string ToString() => $"{Value}";
+        }
+
+        [Theory]
+        [InlineData(9, 25, 32)]
+        [InlineData(10, 1618, 8317)]
+        [InlineData(13, 7999, 146373)]
+        [InlineData(17, 1104, 2764)]
+        [InlineData(21, 6111, 54718)]
+        [InlineData(30, 5807, 37305)]
+        [InlineData(465, 71498, 383475)] // Solution
+        public void Problem1_Solution(int numPlayers, int numMarbles, int answer)
+        {
+            var players = new int[numPlayers];
+            var getCurrentPlayer = Generators.Cycler(numPlayers);
+            var currentMarble = new Marble(0);
+
+            for (var i = 1; i <= numMarbles; i++)
+            {
+                var currentPlayer = getCurrentPlayer();
+                if (i % 23 != 0)
+                {
+                    var marble = new Marble(i);
+                    currentMarble.Insert(marble);
+                    currentMarble = marble;
+                }
+                else
+                {
+                    var marble = currentMarble.RemoveAntiClockwise(7);
+                    players[currentPlayer] += (i + marble.Value);
+                    currentMarble = marble.Clockwise;
+                }
+            }
+
+            players.Max().Should().Be(answer);
+        }
+    }
+}
---
+++ Utils/Generators.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Utils
+{
+    public static class Generators
+    {
+        public static Func<int> Cycler(int range)
+        {
+            var current = 0;
+            return () => current++ % range;
+        }
+    }
+}
