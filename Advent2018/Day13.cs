b4df62aba1319c14d581d7112d6582cbab6ce70c
Adrian O'Grady <elpollouk@users.noreply.github.com>
2018-12-13 12:30:07 +0000

    Day 13, part 1 solution

--- Advent2018/Day13.cs
+++ Advent2018/Day13.cs
@@ -1,6 +1,7 @@
 ﻿using FluentAssertions;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,7 @@ namespace Advent2018
             public int y;
             public int dX;
             public int dY;
+            public int crossingCount = 0;
 
             public Cart(int x, int y, char initialDirection)
             {
@@ -51,6 +53,40 @@ namespace Advent2018
                 }
             }
 
+            public void Step(char currentLocation)
+            {
+                switch (currentLocation)
+                {
+                    case '/':
+                        if (dX == 0)
+                            TurnRight();
+                        else
+                            TurnLeft();
+                        break;
+
+                    case '\\':
+                        if (dX == 0)
+                            TurnLeft();
+                        else
+                            TurnRight();
+                        break;
+
+                    case '+':
+                        UpdateCrossing();
+                        break;
+
+                    case '.':
+                        break;
+
+                    default:
+                        Oh.ShttingHell();
+                        break;
+                }
+
+                x += dX;
+                y += dY;
+            }
+
             public void TurnLeft()
             {
                 if (dX == 0)
@@ -79,7 +115,29 @@ namespace Advent2018
                 }
             }
 
+            public void UpdateCrossing()
+            {
+                switch (crossingCount)
+                {
+                    case 0:
+                        TurnLeft();
+                        crossingCount = 1;
+                        break;
+
+                    case 1:
+                        crossingCount = 2;
+                        break;
+
+                    case 2:
+                        TurnRight();
+                        crossingCount = 0;
+                        break;
+                }
+            }
+
             public bool IsCollided(Cart other) => x == other.x && y == other.y;
+
+            public override string ToString() => $"{x}, {y}";
         }
 
         Cart GetCollidedCart(IList<Cart> carts)
@@ -88,6 +146,84 @@ namespace Advent2018
                 for (var j = i + 1; j < carts.Count; j++)
                     if (carts[i].IsCollided(carts[j]))
                         return carts[i];
+
+            return null;
+        }
+
+        bool HasCollided(IList<Cart> carts, Cart cart)
+        {
+            foreach (var other in carts)
+                if (cart != other && cart.IsCollided(other))
+                    return true;
+
+            return false;
+        }
+
+        (char[,], Cart[]) LoadSimulation(string inputFile)
+        {
+            var lines = FileIterator.LoadLines<string>(inputFile);
+            var width = lines[0].Length;
+            var height = lines.Length;
+            var cavern = new char[width, height];
+            var carts = new List<Cart>();
+
+            foreach (var(x, y) in Generators.Rectangle(width, height))
+            {
+                var thing = lines[y][x];
+                switch (thing)
+                {
+                    case '<':
+                    case '>':
+                    case '^':
+                    case 'V':
+                    case 'v':
+                        carts.Add(new Cart(x, y, thing));
+                        cavern[x, y] = '.';
+                        break;
+
+                    case '-':
+                    case '|':
+                        cavern[x, y] = '.';
+                        break;
+
+                    case '+':
+                    case '/':
+                    case '\\':
+                    case ' ':
+                        cavern[x, y] = thing;
+                        break;
+
+                    default:
+                        Oh.Bollocks();
+                        break;
+                }
+            }
+
+            return (cavern, carts.ToArray());
+        }
+
+        void DebugDumpCavern(char[,] cavern)
+        {
+            foreach (var (x, y) in cavern.Rectangle())
+            {
+                if (x == 0)
+                    Debug.WriteLine("");
+
+                Debug.Write(cavern[x, y]);
+            }
+            Debug.WriteLine("");
+        }
+
+        Cart Step(char[,] cavern, Cart[] carts)
+        {
+            var orderedCarts = carts.OrderBy(c => c.y).ThenBy(c => c.x);
+
+            foreach (var cart in orderedCarts)
+            {
+                cart.Step(cavern[cart.x, cart.y]);
+                if (HasCollided(carts, cart))
+                    return cart;
+            }
 
             return null;
         }
@@ -133,6 +269,20 @@ namespace Advent2018
             collided.Should().NotBeNull();
             collided.x.Should().Be(expectedX);
             collided.y.Should().Be(expectedY);
+        }
+
+        [Theory]
+        [InlineData(7, 3, "Data/Day13-Test.txt")]
+        [InlineData(113, 136, "Data/Day13.txt")] // Solution
+        void Problem1(int expectedX, int expectedY, string inputFile)
+        {
+            var (cavern, carts) = LoadSimulation(inputFile);
+            Cart collidedCart = null;
+            while (collidedCart == null)
+                collidedCart = Step(cavern, carts);
+
+            collidedCart.x.Should().Be(expectedX);
+            collidedCart.y.Should().Be(expectedY);
         }
     }
 }
