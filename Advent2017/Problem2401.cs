a211d9bb552cc0bfe45d3d702c43190083c51841
Adrian O'Grady <elpollouk@users.noreply.github.com>
2019-11-23 18:23:37 +0000

    2017, Day 24, Part 1

---
+++ Advent2017/Problem2401.cs
@@ -0,0 +1,109 @@
+﻿using Utils;
+using FluentAssertions;
+using Xunit;
+using System.Collections.Generic;
+
+namespace Advent2017
+{
+    public class Problem2401
+    {
+        struct Connector
+        {
+            public Connector(int id, int input, int output)
+            {
+                Id = id;
+                Input = input;
+                Output = output;
+                Value = input + output;
+            }
+
+            public override string ToString()
+            {
+                return $"{Input}/{Output}";
+            }
+
+            public readonly int Id;
+            public readonly int Input;
+            public readonly int Output;
+            public readonly int Value;
+        }
+
+        Dictionary<int, List<Connector>> LoadConnectors(string datafile)
+        {
+            var id = 0;
+            var connectors = new Dictionary<int, List<Connector>>();
+            FileIterator.ForEachLine(datafile, (string line) =>
+            {
+                var sockets = line.Split('/');
+                var input = int.Parse(sockets[0]);
+                var output = int.Parse(sockets[1]);
+
+                id++;
+                var connector = new Connector(id, input, output);
+                var outputs = connectors.GetOrCreate(input, () => new List<Connector>());
+                outputs.Add(connector);
+
+                if (connector.Input != connector.Output)
+                {
+                    connector = new Connector(id, output, input);
+                    outputs = connectors.GetOrCreate(output, () => new List<Connector>());
+                    outputs.Add(connector);
+                }
+            });
+
+            foreach (var outputs in connectors.Values)
+                outputs.Sort((a, b) => b.Output - a.Output);
+
+            return connectors;
+        }
+
+        private HashSet<int> _used;
+
+        void Walk(Dictionary<int, List<Connector>> connectors, int output, int currentValue, ref int maxValue)
+        {
+            var validConnectors = connectors.GetOrDefault(output);
+            if (validConnectors == null) return;
+
+
+            for (var i = 0; i < validConnectors.Count; i++)
+            {
+                var connector = validConnectors[i];
+                var id = connector.Id;
+                if (!_used.Contains(id))
+                {
+                    _used.Add(id);
+                    currentValue += connector.Value;
+                    if (maxValue < currentValue)
+                        maxValue = currentValue;
+
+                    Walk(connectors, connector.Output, currentValue, ref maxValue);
+
+                    currentValue -= connector.Value;
+                    _used.Remove(id);
+                }
+            }
+        }
+
+        int MaximumBridge(string datafile)
+        {
+            _used = new HashSet<int>();
+            var connectors = LoadConnectors(datafile);
+            int maxValue = 0;
+
+            Walk(connectors, 0, 0, ref maxValue);
+
+            return maxValue;
+        }
+
+        [Theory]
+        [InlineData("Data/2401-example.txt", 31)]
+        [InlineData("Data/2401.txt", 1940)]
+        public void Part1(string datafile, int answer) => MaximumBridge(datafile).Should().Be(answer);
+
+        [Fact]
+        public void Part2()
+        {
+
+        }
+    }
+}
