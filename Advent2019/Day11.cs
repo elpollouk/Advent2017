10c61fecbc50dd3f1882d4ceb02644631b5dd7f9
Adrian O'Grady <elpollouk@gmail.com>
2019-12-11 12:28:23 -0800

    Refactored my robot into a Turtle for fture reuse

--- Advent2019/Day11.cs
+++ Advent2019/Day11.cs
@@ -9,15 +9,17 @@ namespace Advent2019
 {
     public class Day11
     {
-        class Robot
-        {
-            public const int BLACK = 0;
-            public const int WHITE = 1;
+        const int BLACK = 0;
+        const int WHITE = 1;
 
-            const int FACING_UP = 0;
-            const int FACING_RIGHT = 1;
-            const int FACING_DOWN = 2;
-            const int FACING_LEFT = 3;
+        class Turtle
+        {
+            public const int FACING_UP = 0;
+            public const int FACING_RIGHT = 1;
+            public const int FACING_DOWN = 2;
+            public const int FACING_LEFT = 3;
+            public const int TURN_LEFT = 0;
+            public const int TURN_RIGHT = 1;
 
             enum State
             {
@@ -25,48 +27,38 @@ namespace Advent2019
                 MOVE
             }
 
-            public readonly Dictionary<(int, int), int> Hull = new Dictionary<(int, int), int>();
+            private readonly Dictionary<(int, int), int> _paper;
             private (int x, int y) _pos = (0, 0);
             private int _facing = FACING_UP;
             private State _state = State.PAINT;
 
-            public long Camera()
+            public Turtle(Dictionary<(int, int), int> paper)
             {
-                return Hull.GetOrDefault(_pos, BLACK);
+                _paper = paper;
             }
 
-            public void Act(long action)
+            public void TurnLeft()
             {
-                switch (_state)
-                {
-                    case State.PAINT:
-                        Hull[_pos] = (int)action;
-                        _state = State.MOVE;
-                        break;
-
-                    case State.MOVE:
-                        Rotate(action);
-                        Move();
-                        _state = State.PAINT;
-                        break;
-
-                    default:
-                        throw new InvalidOperationException("Invalid robot state");
-                }
+                _facing--;
+                if (_facing == -1) _facing = FACING_LEFT;
             }
 
-            void Rotate(long direction)
+            public void TurnRight()
+            {
+                _facing++;
+                if (_facing == 4) _facing = FACING_UP;
+            }
+
+            public void Turn(int direction)
             {
                 switch (direction)
                 {
-                    case 0:
-                        _facing--;
-                        if (_facing == -1) _facing = FACING_LEFT;
+                    case TURN_LEFT:
+                        TurnLeft();
                         break;
 
-                    case 1:
-                        _facing++;
-                        if (_facing == 4) _facing = FACING_UP;
+                    case TURN_RIGHT:
+                        TurnRight();
                         break;
 
                     default:
@@ -74,7 +66,7 @@ namespace Advent2019
                 }
             }
 
-            void Move()
+            public void Forward()
             {
                 _pos = _facing switch
                 {
@@ -85,6 +77,33 @@ namespace Advent2019
                     _ => throw new InvalidOperationException("Invalid facing")
                 };
             }
+
+            public void Paint(int ink)
+            {
+                _paper[_pos] = ink;
+            }
+
+            public long Observe() => _paper.GetOrDefault(_pos, BLACK);
+
+            public void Act(long action)
+            {
+                switch (_state)
+                {
+                    case State.PAINT:
+                        Paint((int)action);
+                        _state = State.MOVE;
+                        break;
+
+                    case State.MOVE:
+                        Turn((int)action);
+                        Forward();
+                        _state = State.PAINT;
+                        break;
+
+                    default:
+                        throw new InvalidOperationException("Invalid robot state");
+                }
+            }
         }
 
         [Theory]
@@ -93,13 +112,14 @@ namespace Advent2019
         {
             var prog = FileIterator.LoadCSV<long>(input);
             var vm = IntCode.CreateVM(prog);
-            var robot = new Robot();
-            vm.State.Input = robot.Camera;
+            var hull = new Dictionary<(int, int), int>();
+            var robot = new Turtle(hull);
+            vm.State.Input = robot.Observe;
             vm.State.Output = robot.Act;
 
             vm.Execute();
 
-            robot.Hull.Count.Should().Be(answer);
+            hull.Count.Should().Be(answer);
         }
 
         [Theory]
@@ -108,26 +128,27 @@ namespace Advent2019
         {
             var prog = FileIterator.LoadCSV<long>(input);
             var vm = IntCode.CreateVM(prog);
-            var robot = new Robot();
-            robot.Hull[(0, 0)] = Robot.WHITE;
-            vm.State.Input = robot.Camera;
+            var hull = new Dictionary<(int, int), int>();
+            hull[(0, 0)] = WHITE;
+            var robot = new Turtle(hull);
+            vm.State.Input = robot.Observe;
             vm.State.Output = robot.Act;
 
             vm.Execute();
 
-            robot.Hull.Count.Should().Be(answer);
+            hull.Count.Should().Be(answer);
 
-            var minX = robot.Hull.Keys.Select(k => k.Item1).Min();
-            var maxX = robot.Hull.Keys.Select(k => k.Item1).Max();
-            var minY = robot.Hull.Keys.Select(k => k.Item2).Min();
-            var maxY = robot.Hull.Keys.Select(k => k.Item2).Max();
+            var minX = hull.Keys.Select(k => k.Item1).Min();
+            var maxX = hull.Keys.Select(k => k.Item1).Max();
+            var minY = hull.Keys.Select(k => k.Item2).Min();
+            var maxY = hull.Keys.Select(k => k.Item2).Max();
 
             var output = "";
             for (var y = minY; y <= maxY; y++)
             {
                 for (var x = minX; x <= maxX; x++)
                 {
-                    output += robot.Hull.GetOrDefault((x, y), Robot.BLACK) == Robot.BLACK ? ' ' : '#';
+                    output += hull.GetOrDefault((x, y), BLACK) == BLACK ? ' ' : '#';
                 }
                 output += '\n';
             }
