1b0dd8f37272b067f9c34bbebc7a03cd6decb7cd
Adrian O'Grady <elpollouk@users.noreply.github.com>
2020-11-30 21:08:27 +0000

    Added 2020 project

---
+++ Advent2020/Day01.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Advent2020
+{
+    public class Day01
+    {
+        [Theory]
+        [InlineData(1, 0)]
+        public void Part1(int a, int b)
+        {
+            (a - 1).Should().Be(b);
+        }
+    }
+}
