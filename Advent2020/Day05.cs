df64aed547fcd2738ebd9804d9e85885d6c94e94
Adrian O'Grady <elpollouk@users.noreply.github.com>
2020-12-05 09:30:15 +0000

    Day 5 solutions

---
+++ Advent2020/Day05.cs
@@ -0,0 +1,77 @@
+﻿using FluentAssertions;
+using System.Collections.Generic;
+using System.Linq;
+using Utils;
+using Xunit;
+
+
+namespace Advent2020
+{
+    public class Day05
+    {
+        int ParseSeatId(string raw)
+        {
+            // Decode row
+            var min = 0;
+            var max = 128;
+            var mid = 64;
+
+            for (var i = 0; i < 7; i++)
+            {
+                if (raw[i] == 'F') max = mid;
+                else min = mid;
+                mid = (min + max) / 2;
+            }
+            var row = mid;
+
+            // Decode column
+            min = 0;
+            max = 8;
+            mid = 4;
+
+            for (var i = 7; i < 10; i++)
+            {
+                if (raw[i] == 'L') max = mid;
+                else min = mid;
+                mid = (min + max) / 2;
+            }
+
+            // Encode seat id
+            return (row * 8) + mid;
+        }
+
+        [Theory]
+        [InlineData("FBFBBFFRLR", 357)]
+        [InlineData("BFFFBBFRRR", 567)]
+        [InlineData("FFFBBBFRRR", 119)]
+        [InlineData("BBFFBBFRLL", 820)]
+        public void Test_ParseSeatId(string input, int expected) => ParseSeatId(input).Should().Be(expected);
+
+        [Fact]
+        public void Problem1()
+        {
+            FileIterator.Lines("Data/Day05.txt")
+                .Select(ParseSeatId)
+                .Max()
+                .Should().Be(911);
+        }
+
+        [Fact]
+        public void Problem2()
+        {
+            var seats = new bool[128 * 8];
+            FileIterator.ForEachLine("Data/Day05.txt", (string line) => seats[ParseSeatId(line)] = true);
+
+            var searching = false;
+            var seatId = 0;
+            foreach (var taken in seats)
+            {
+                if (searching && !taken) break;
+                searching = taken;
+                seatId++;
+            }
+
+            seatId.Should().Be(629);
+        }
+    }
+}
