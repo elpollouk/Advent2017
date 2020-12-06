3feef90ee129ae8c3282ac31535a6487a6307bfa
Adrian O'Grady <elpollouk@users.noreply.github.com>
2020-12-06 11:12:27 +0000

    Day 6 solutions

---
+++ Advent2020/Day06.cs
@@ -0,0 +1,68 @@
+﻿using FluentAssertions;
+using System.Collections.Generic;
+using System.Linq;
+using Utils;
+using Xunit;
+
+using Answers = System.Collections.Generic.Dictionary<char, long>;
+
+namespace Advent2020
+{
+    public class Day06
+    {
+        const char GROUP_SIZE = '#';
+        List<Answers> LoadAnswers(string input)
+        {
+            var answers = new List<Answers>();
+            var currentGroup = new Answers();
+            var currentGroupSize = 0L;
+            answers.Add(currentGroup);
+
+            FileIterator.ForEachLine(input, (string line) =>
+            {
+                if (line == "")
+                {
+                    currentGroup[GROUP_SIZE] = currentGroupSize;
+                    currentGroup = new Answers();
+                    answers.Add(currentGroup);
+                    currentGroupSize = 0;
+                    return;
+                }
+
+                currentGroupSize++;
+                foreach (var c in line)
+                    currentGroup.Increment(c);
+            });
+
+            currentGroup[GROUP_SIZE] = currentGroupSize;
+            return answers;
+        }
+
+        [Theory]
+        [InlineData("Data/Day06_test.txt", 11)]
+        [InlineData("Data/Day06.txt", 7027)]
+        public void Problem1(string input, int expected)
+        {
+            LoadAnswers(input)
+                .Select(a => a.Keys.Count() - 1) // Exclude the GROUP_SIZE key from the count
+                .Sum()
+                .Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("Data/Day06_test.txt", 6)]
+        [InlineData("Data/Day06.txt", 3579)]
+        public void Problem2(string input, int expected)
+        {
+            LoadAnswers(input)
+                // Filter down to keys that have the same count as the group size excluding the GROUP_SIZE key iteslf
+                .Select(a => a.Keys
+                    .Where(k => k != GROUP_SIZE)
+                    .Where(k => a[k] == a[GROUP_SIZE])
+                    .Count()
+                )
+                .Sum()
+                .Should().Be(expected);
+        }
+    }
+}
--- Utils/DictionaryExtensions.cs
+++ Utils/DictionaryExtensions.cs
@@ -22,6 +22,13 @@ namespace Utils
             return value;
         }
 
+        public static long Increment<K>(this IDictionary<K, long> dic, K key)
+        {
+            var count = dic.GetOrDefault(key, 0) + 1;
+            dic[key] = count;
+            return count;
+        }
+
         public static void Index<K, V>(this IDictionary<K, V> dic, IEnumerable<V> collection, Func<V, K> getKey)
         {
             foreach (var item in collection)
