6c4317589f074a4757b916c871f0a9e076ba6f7c
Adrian O'Grady <adrian.ogrady@jagex.com>
2020-12-09 09:31:59 +0000

    Day 9 solutions

--- Advent2020/Day01.cs
+++ Advent2020/Day01.cs
@@ -7,9 +7,9 @@ namespace Advent2020
 {
     public class Day01
     {
-        private int Calc2(IEnumerable<int> values, int target = 2020)
+        public static long Calc2(IEnumerable<long> values, long target = 2020)
         {
-            var valuesSet = new HashSet<int>(values);
+            var valuesSet = new HashSet<long>(values);
 
             foreach (var value in values)
             {
@@ -20,7 +20,7 @@ namespace Advent2020
             return -1;
         }
 
-        private int Calc3(IEnumerable<int> values)
+        private long Calc3(IEnumerable<long> values)
         {
             foreach (var value in values)
             {
@@ -33,15 +33,15 @@ namespace Advent2020
         }
 
         [Theory]
-        [InlineData(514579, new int[] { 1721, 979, 366, 299, 675, 1456 })]
-        public void Part1_Test(int expected, int[] values)
+        [InlineData(514579, new long[] { 1721, 979, 366, 299, 675, 1456 })]
+        public void Part1_Test(int expected, long[] values)
         {
             Calc2(values).Should().Be(expected);
         }
 
         [Theory]
-        [InlineData(241861950, new int[] { 1721, 979, 366, 299, 675, 1456 })]
-        public void Part2_Test(int expected, int[] values)
+        [InlineData(241861950, new long[] { 1721, 979, 366, 299, 675, 1456 })]
+        public void Part2_Test(long expected, long[] values)
         {
             Calc3(values).Should().Be(expected);
         }
@@ -49,14 +49,14 @@ namespace Advent2020
         [Fact]
         public void Part1_Solution()
         {
-            var values = FileIterator.Lines<int>("Data/Day01.txt");
+            var values = FileIterator.Lines<long>("Data/Day01.txt");
             Calc2(values).Should().Be(889779);
         }
 
         [Fact]
         public void Part2_Solution()
         {
-            var values = FileIterator.Lines<int>("Data/Day01.txt");
+            var values = FileIterator.Lines<long>("Data/Day01.txt");
             Calc3(values).Should().Be(76110336);
         }
     }
---
+++ Advent2020/Day09.cs
@@ -0,0 +1,73 @@
+﻿using FluentAssertions;
+using System.Collections.Generic;
+using Utils;
+using Xunit;
+
+namespace Advent2020
+{
+    public class Day09
+    {
+        long FirstInvalid(string input, int windowSize)
+        {
+            var window = new List<long>();
+
+            foreach (var value in FileIterator.Lines<long>(input))
+            {
+                if (window.Count == windowSize)
+                {
+                    var isValid = Day01.Calc2(window, value);
+                    if (isValid < 0) return value;
+                    window.Shift();
+                }
+                window.Add(value);
+            }
+
+            throw new Expletive("Fuck");
+        }
+
+        List<long> ContiguousSet(string input, long target)
+        {
+            var window = new List<long>();
+            var windowSum = 0L;
+
+            foreach (var value in FileIterator.Lines<long>(input))
+            {
+                if (value == target) continue; // I'm not sure if we're meant to search after the target number or not
+                window.Add(value);
+                windowSum += value;
+
+                while (windowSum > target)
+                    windowSum -= window.Shift();
+
+                if (windowSum == target) return window;
+            }
+
+            throw new Expletive("Bollocks");
+        }
+
+        [Theory]
+        [InlineData("Data/Day09_test.txt", 5, 127)]
+        [InlineData("Data/Day09.txt", 25, 133015568)]
+        public void Problem1(string input, int windowSize, long expected)
+        {
+            FirstInvalid(input, windowSize).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("Data/Day09_test.txt", 127, 62)]
+        [InlineData("Data/Day09.txt", 133015568, 16107959)]
+        public void Problem2(string input, long target, long expected)
+        {
+            var set = ContiguousSet(input, target);
+            var min = long.MaxValue;
+            var max = long.MinValue;
+            foreach (var value in set)
+            {
+                if (value < min) min = value;
+                if (value > max) max = value;
+            }
+
+            (min + max).Should().Be(expected);
+        }
+    }
+}
