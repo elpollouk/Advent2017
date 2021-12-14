e9594cbfa34e15dfc9a122330745c5bc70c6c8a7
Adrian O'Grady <elpollouk@gmail.com>
2021-12-14 11:05:45 +0000

    Advent 2021, day 14

--- Advent2021/Day14.cs
+++ Advent2021/Day14.cs
@@ -8,34 +8,12 @@ namespace Advent2021
 {
     public class Day14
     {
-        class Node
+        static (string template, Dictionary<(char, char), char> rules) LoadData(string filename)
         {
-            public char Id { get; init; }
-            public Node Next;
-        }
-
-        static (Node template, Dictionary<(char, char), char> rules) LoadData(string filename)
-        {
-            Node template = null;
-            Node last = null;
             Dictionary<(char, char), char> rules = new();
 
             var reader = FileIterator.CreateLineReader(filename);
-            var line = reader();
-            foreach (var c in line)
-            {
-                Node newNode = new() { Id = c };
-                if (template == null)
-                {
-                    template = newNode;
-                    last = newNode;
-                }
-                else
-                {
-                    last.Next = newNode;
-                    last = newNode;
-                }
-            }
+            var template = reader();
             reader();
 
             foreach (var mapping in FileIterator.Lines(reader))
@@ -47,34 +25,50 @@ namespace Advent2021
             return (template, rules);
         }
 
-        static void Expand(Node poly, Dictionary<(char, char), char> rules)
+        static void Add(long[] acc, long[] d)
         {
-            while (poly.Next != null)
+            for (int i = 0; i < acc.Length; i++)
+                acc[i] += d[i];
+        }
+
+        static readonly Dictionary<(char, char, int), long[]> cache = new();
+
+        static void Expand(Dictionary<(char, char), char> rules, long[] counts, char c1, char c2, int iteration)
+        {
+            if (iteration == 0) return;
+            if (cache.TryGetValue((c1, c2, iteration), out long[] delta))
             {
-                var c1 = poly.Id;
-                var c2 = poly.Next.Id;
-                var newId = rules[(c1, c2)];
-                var newNode = new Node { Id = newId, Next = poly.Next };
-                poly.Next = newNode;
-                poly = newNode.Next;
+                Add(counts, delta);
+                return;
             }
+
+            var cM = rules[(c1, c2)];
+
+            delta = new long[26];
+            delta[cM - 'A']++;
+            Expand(rules, delta, c1, cM, iteration - 1);
+            Expand(rules, delta, cM, c2, iteration - 1);
+            Add(counts, delta);
+
+            cache[(c1, c2, iteration)] = delta;
         }
 
         [Theory]
+        [InlineData("Data/Day14_Test.txt", 2, 5)]
         [InlineData("Data/Day14_Test.txt", 10, 1588)]
+        [InlineData("Data/Day14_Test.txt", 40, 2188189693529)]
         [InlineData("Data/Day14.txt", 10, 3095)]
+        [InlineData("Data/Day14.txt", 40, 3152788426516)]
         public void Part1(string filename, int iterations, long expectedAnswer)
         {
+            cache.Clear();
             (var p, var rules) = LoadData(filename);
-            while (iterations --> 0)
-            {
-                Expand(p, rules);
-            }
-
             var counts = new long[26];
-            for (var node = p; node != null; node = node.Next)
+            counts[p[0] - 'A'] = 1;
+            for (int i = 1; i < p.Length; i++)
             {
-                counts[node.Id - 'A']++;
+                counts[p[i] - 'A']++;
+                Expand(rules, counts, p[i - 1], p[i], iterations);
             }
 
             (var min, var max) = counts.Where(v => v != 0).MinAndMax();
