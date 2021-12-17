a1c8b604483a6ef8acc9474cd5bef59620c15035
Adrian O'Grady <elpollouk@users.noreply.github.com>
2021-12-17 22:30:53 +0000

    Advent 2019, day 15, part 1

---
+++ Advent2019/Day15.cs
@@ -0,0 +1,89 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Utils;
+using Utils.Alogrithms;
+using Xunit;
+
+namespace Advent2019
+{
+    public class Day15
+    {
+        class Map :Astar.IGraphAdapter<(int x, int y)>
+        {
+            private readonly char[,] _map;
+
+            public Map(char[,] map)
+            {
+                _map = map;
+            }
+
+            public IEnumerable<(int x, int y)> GetLinked((int x, int y) node)
+            {
+                foreach (var pos in _map.GetAdjecentPos(node.x, node.y))
+                {
+                    if (_map[pos.x, pos.y] != '#')
+                    {
+                        yield return pos;
+                    }
+                }
+            }
+
+            public int GetMoveCost((int x, int y) from, (int x, int y) to)
+            {
+                return 1;
+            }
+
+            public int GetScore((int x, int y) from, (int x, int y) to)
+            {
+                return Math.Abs(to.x - from.x) + Math.Abs(to.y - from.y);
+            }
+        }
+
+        static int MoveToAction((int x, int y) from, (int x, int y) to)
+        {
+            if (to == (from.x, from.y - 1)) return 1;
+            if (to == (from.x, from.y + 1)) return 2;
+            if (to == (from.x - 1, from.y)) return 3;
+            if (to == (from.x + 1, from.y)) return 4;
+            throw new InvalidOperationException();
+        }
+
+        [Fact]
+        public void Part1()
+        {
+            var start = (x: -1, y: -1);
+            var exit = (x: -1, y: -1);
+
+            var grid = FileIterator.LoadGrid<char>("Data/Day15.txt", (c, x, y) =>
+            {
+                if (c == 'D') start = (x, y);
+                else if (c == 'X') exit = (x, y);
+                
+                if (c != '#')  c = ' ';
+
+                return c;
+            });
+
+            var map = new Map(grid);
+
+            var path = Astar.FindPath(map, start, exit).ToArray();
+            path.Length.Should().Be(305);
+            var actions = "";
+            for (var i = 1; i < path.Length; i++)
+            {
+                if (i != 1) actions += ',';
+                actions += MoveToAction(path[i - 1], path[i]);
+            }
+            Debug.WriteLine(actions);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+
+        }
+    }
+}
---
+++ DedicatedSolutions/2019_Day15/Program.cs
@@ -0,0 +1,171 @@
+﻿using Advent2019;
+using System;
+using System.Collections.Generic;
+using Utils;
+using Utils.VM;
+using static Advent2019.IntCode;
+
+namespace _2019_Day15
+{
+    class Program
+    {
+        static int x;
+        static int y;
+        static int o2x = -1;
+        static int o2y = -1;
+        static int lastAction = 0;
+        static Queue<int> queudActions = new();
+
+        static Executor<VmState, int, (long, long, long)> vm;
+
+        static void Main(string[] args)
+        {
+            var width = Console.WindowWidth;
+            var height = Console.WindowHeight;
+
+            Console.Clear();
+            Console.CursorVisible = false;
+            Console.SetCursorPosition(0, 0);
+            Console.Write($"w={width}, h={height}");
+
+            x = width / 2;
+            y = 30;
+
+            var prog = FileIterator.LoadCSV<int>("prog.txt");
+            vm = IntCode.CreateVM(prog);
+            vm.State.Input = Step;
+
+            Console.SetCursorPosition(x, y);
+            Console.Write('D');
+
+            vm.Execute();
+        }
+
+        static long Step()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write('·');
+
+            if (vm.State.OutputQueue.Count != 0)
+            {
+                var result = vm.State.OutputQueue.Dequeue();
+                EvaluateResult((int)result);
+            }
+
+            Console.SetCursorPosition(x, y);
+            Console.Write('D');
+
+            if (o2x != -1)
+            {
+                Console.SetCursorPosition(o2x, o2y);
+                Console.Write('X');
+            }
+
+            if (queudActions.Count != 0)
+            {
+                lastAction = queudActions.Dequeue();
+                return lastAction;
+            }
+
+            lastAction = 0;
+            do
+            {
+                var info = Console.ReadKey(true);
+                switch (info.Key)
+                {
+                    case ConsoleKey.X:
+                        Environment.Exit(0);
+                        break;
+
+                    case ConsoleKey.Q:
+                        SendAction(1);
+                        queudActions.Enqueue(2);
+                        queudActions.Enqueue(3);
+                        queudActions.Enqueue(4);
+                        break;
+
+                    case ConsoleKey.UpArrow:
+                        SendAction(1);
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        SendAction(2);
+                        break;
+
+                    case ConsoleKey.LeftArrow:
+                        SendAction(3);
+                        break;
+
+                    case ConsoleKey.RightArrow:
+                        SendAction(4);
+                        break;
+                }
+            }
+            while (lastAction == 0);
+
+            return lastAction;
+        }
+
+        static void EvaluateResult(int result)
+        {
+            int newX, newY;
+
+            switch (lastAction)
+            {
+                case 0:
+                    newX = x;
+                    newY = y;
+                    break;
+
+                case 1:
+                    newX = x;
+                    newY = y - 1;
+                    break;
+
+                case 2:
+                    newX = x;
+                    newY = y + 1;
+                    break;
+
+                case 3:
+                    newX = x - 1;
+                    newY = y;
+                    break;
+
+                case 4:
+                    newX = x + 1;
+                    newY = y;
+                    break;
+
+                default:
+                    throw new InvalidOperationException($"invalid action: {lastAction}");
+            }
+
+            if (result == 1 || result == 2)
+            {
+                x = newX;
+                y = newY;
+
+                if (result == 2)
+                {
+                    o2x = x;
+                    o2y = y;
+                }
+            }
+            else if (result == 0)
+            {
+                Console.SetCursorPosition(newX, newY);
+                Console.Write('#');
+            }
+            else
+            {
+                throw new InvalidOperationException($"Invalid result: {result}");
+            }
+        }
+
+        static void SendAction(int action)
+        {
+            lastAction = action;
+        }
+    }
+}
