05b203f8e06ad8753dd857840a8df6831cc7d31a
Adrian O'Grady <elpollouk@users.noreply.github.com>
2022-12-12 10:57:48 +0000

    Advent 2022, test 12

---
+++ Advent2022/Day12.cs
@@ -0,0 +1,85 @@
+﻿using FluentAssertions;
+using System.Collections.Generic;
+using Utils;
+using Utils.Alogrithms;
+using Xunit;
+
+namespace Advent2022
+{
+    public class Day12
+    {
+        class Adapter : GridAdapterBase<int>
+        {
+            public Adapter(int[,] grid) : base(grid) {}
+
+            public override IEnumerable<(int x, int y)> GetLinked((int x, int y) node)
+            {
+                var currentHeight = Grid[node.x, node.y];
+                foreach (var pos in Grid.GetAdjacentPos(node.x, node.y))
+                {
+                    var destHeight = Grid[pos.x, pos.y];
+                    if (destHeight <= currentHeight + 1)
+                    {
+                        yield return pos;
+                    }
+                }
+            }
+        }
+
+        (Adapter, (int,int), (int,int)) LoadGrid(string filename)
+        {
+            (int x, int y) start = (-1, -1);
+            (int x, int y) end = (-1, -1);
+            var grid = FileIterator.LoadGrid(filename, (c, x, y) =>
+            {
+                if (c == 'S')
+                {
+                    start = (x, y);
+                    return 0;
+                }
+                else if (c == 'E')
+                {
+                    end = (x, y);
+                    return 25;
+                }
+                return c - 'a';
+            });
+
+            return (new(grid), start, end);
+        }
+
+        int GetPathLength(Adapter adapter, (int,int) start, (int,int) end)
+        {
+            var path = Astar.FindPath(adapter, start, end);
+            if (path == null) return int.MaxValue;
+            return path.Count - 1;
+        }
+
+        [Theory]
+        [InlineData("Data/Day12_Test.txt", 31)]
+        [InlineData("Data/Day12.txt", 361)]
+        public void Part1(string filename, int expectedAnswer)
+        {
+            var (adapter, start, end) = LoadGrid(filename);
+            GetPathLength(adapter, start, end).Should().Be(expectedAnswer);
+        }
+
+        [Theory]
+        [InlineData("Data/Day12_Test.txt", 29)]
+        [InlineData("Data/Day12.txt", 354)]
+        public void Part2(string filename, int expectedAnswer)
+        {
+            var (adapter, _, end) = LoadGrid(filename);
+            int shortest = int.MaxValue;
+
+            foreach (var pos in adapter.Grid.Rectangle())
+            {
+                if (adapter.Grid[pos.x, pos.y] != 0) continue;
+                var length = GetPathLength(adapter, pos, end);
+                if (length < shortest) shortest = length;
+            }
+
+            shortest.Should().Be(expectedAnswer);
+        }
+    }
+}
---
+++ Utils/GridAdapterBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Utils.Alogrithms;
+
+namespace Utils
+{
+    public abstract class GridAdapterBase<T> : Astar.IGraphAdapter<(int x, int y)>
+    {
+        public readonly T[,] Grid;
+
+        public GridAdapterBase(T[,] grid)
+        {
+            Grid = grid;
+        }
+
+        public abstract IEnumerable<(int x, int y)> GetLinked((int x, int y) node);
+
+        public int GetMoveCost((int x, int y) from, (int x, int y) to)
+        {
+            return 1;
+        }
+
+        public int GetScore((int x, int y) from, (int x, int y) to)
+        {
+            return Math.Abs(from.x - to.x) + Math.Abs(from.y - to.y);
+        }
+    }
+}
