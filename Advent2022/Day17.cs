b144ad5d6c37ecbe42ee0bcc9795d712baacde95
Adrian O'Grady <elpollouk@users.noreply.github.com>
2022-12-19 21:36:31 +0000

    Advent 2022, day 17, part 2

--- Advent2022/Day17.cs
+++ Advent2022/Day17.cs
@@ -118,6 +118,12 @@ namespace Advent2022
             }
         }
 
+        Func<Rock> CreateRocks()
+        {
+            int i = 0;
+            return () => Rocks.GetAtMod(i++);
+        }
+
         Func<int> CreateWind(string filename)
         {
             var wind = FileIterator.Lines(filename).First();
@@ -127,6 +133,28 @@ namespace Advent2022
                 var c = wind.GetAtMod(i++);
                 return c == '<' ? -1 : 1;
             };
+        }
+
+        ulong HashTop32(HashSet<(int,int)> space, int top)
+        {
+            ulong hash = 0xDEADBEEFDEADBEEF;
+            int y = top - 1;
+            for (int i = 0; i < 4; i++)
+            {
+                ulong value = 0;
+                for (int j = 0; j < 8; j++)
+                {
+                    for (int x = 0; x < 7; x++)
+                    {
+                        value <<= 1;
+                        value |= space.Contains((x, y)) ? 1UL : 0UL;
+                    }
+                    y--;
+                }
+                hash *= value ^ 0xFFFFFFFFFFFFFFFF;
+            }
+
+            return hash;
         }
 
         [Theory]
@@ -149,11 +177,50 @@ namespace Advent2022
         }
 
         [Theory]
-        [InlineData("Data/Day17_Test.txt", 1514285714288)]
-        [InlineData("Data/Day17.txt", 0)]
-        public void Part2(string filename, long expectedAnswer)
+        [InlineData("Data/Day17_Test.txt", 2022, 3068)]
+        [InlineData("Data/Day17_Test.txt", 1000000000000, 1514285714288)]
+        [InlineData("Data/Day17.txt", 1000000000000, 1537175792495)]
+        public void Part2(string filename, long numDrops, long expectedAnswer)
         {
+            var wind = CreateWind(filename);
+            var nextRock = CreateRocks();
+            HashSet<(int, int)> space = new();
+            Dictionary<ulong, (int, int)> seen = new();
+            int top = 0;
 
+            int cycleLength = 0;
+            int cycleDelta = 0;
+            for (int i = 0; i < numDrops; i++)
+            {
+                var rock = nextRock();
+                var t = Drop(rock, top + 3, space, wind);
+                if (top < t) top = t;
+
+                var hash = HashTop32(space, top);
+                if (seen.TryGetValue(hash, out (int seen, int top) last))
+                {
+                    cycleLength = i - last.seen;
+                    cycleDelta = top - last.top;
+                    numDrops -= i;
+                    break;
+                }
+                else
+                {
+                    seen[hash] = (i, top);
+                }
+            }
+
+            long numCycles = numDrops / cycleLength;
+            long remainderCycles = (numDrops % cycleLength) - 1;
+            for (int i = 0; i < remainderCycles; i++)
+            {
+                var rock = nextRock();
+                var t = Drop(rock, top + 3, space, wind);
+                if (top < t) top = t;
+            }
+
+            long ltop = top + (numCycles * cycleDelta);
+            ltop.Should().Be(expectedAnswer);
         }
     }
 }
